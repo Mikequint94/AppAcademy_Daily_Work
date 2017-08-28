f9fb89e882ae3bb3ae0be58ee441fb20f0ac3af6
Michael Quint <mjq8@cornell.edu>
2017-08-28 11:38:53 -0700

    second attempt to start proj

--- Week5/W5D1/goals_app2/config/application.rb
+++ Week5/W5D1/goals_app2/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GoalsApp
+module GoalsApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -14,15 +14,5 @@ module GoalsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.generators do |g|
-      g.test_framework :rspec,
-    :fixtures => false,
-    :view_specs => false,
-    :helper_specs => false,
-    :routing_specs => false,
-    :controller_specs => true,
-    :request_specs => false
-    end
   end
 end
